183530127cf5077aedd45de452f2c56811714e8a
Antonio Niño Díaz <antonio_nd@outlook.com>
2025-10-21 18:21:54 +0100

    image: Add missing copyright notice

--- libndspp/source/common/Image.cpp
+++ libndspp/source/common/Image.cpp
@@ -1,5 +1,6 @@
 /*
   Copyright (C) 2007,2008 Richard Quirk
+  Copyright (C) 2025 Antonio Niño Díaz
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
