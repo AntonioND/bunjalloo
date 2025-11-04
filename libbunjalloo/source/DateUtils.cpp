e9c968e57c4095ce21065191e7869f1624a5a5ee
Antonio Niño Díaz <antonio_nd@outlook.com>
2025-11-04 18:29:20 +0000

    date: Stop using timezone variable
    Use timegm() instead of mktime() so that all dates are in GMT.
    
    According to https://stackoverflow.com/a/75189528
    
        The correct defs to use are -D_XOPEN_SOURCE=1 and -D_GNU_SOURCE=1.
        These are used to conditionally define __XSI_VISIBLE and
        __GNU_VISIBLE respectively in <sys/features.h>. Defining
        __XSI_VISIBLE and __GNU_VISIBLE will not always work because they
        are overridden in <sys/features.h>.

--- libbunjalloo/source/DateUtils.cpp
+++ libbunjalloo/source/DateUtils.cpp
@@ -1,4 +1,3 @@
-#define _XOPEN_SOURCE // Declare strptime() in <time.h>
 #include <time.h>
 
 #include <string>
@@ -7,11 +6,7 @@
 #include "config_defs.h"
 #include <cstring>
 
-#ifdef ARM9
-// time.h doesn't have timezone on the DS.
-static int timezone = 0;
-#endif
-
+// picolibc doesn't implement %Z
 static const char FORMAT[] = "%a, %d %b %Y %H:%M:%S GMT";
 
 static char tospace(char c)
@@ -31,15 +26,13 @@ time_t DateUtils::parseDate(const char *date)
     return 0;
 
   val.tm_isdst = 0;
-  timezone = 0;
-  return mktime(&val);
+  return timegm(&val);
 }
 
 std::string DateUtils::formatTime(time_t t)
 {
   char buffer[140];
   memset(buffer, 0, sizeof(buffer));
-  t -= timezone;
   struct tm *val = gmtime(&t);
   strftime(buffer, sizeof(buffer), FORMAT, val);
   return buffer;
