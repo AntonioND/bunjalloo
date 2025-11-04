bb0104b25ce24178b5c505478ec5c9e2fc5f77fc
Antonio Niño Díaz <antonio_nd@outlook.com>
2025-11-04 01:53:26 +0000

    tests: Test and document split test

--- tests/split/test_split.cpp
+++ tests/split/test_split.cpp
@@ -3,8 +3,22 @@
 #include <cstdio>
 #include <cstdlib>
 
+// This compares std::split() with python's split()
+//
+// Run the program like this:
+//
+//     ./test.elf 123/124/1566/777// /
+//
+// You'll get something similar to this:
+//
+//     ['123', '124', '1566', '777', '', '']
+//     ['123', '124', '1566', '777', '', '']
+
 int main(int argc, char *argv[])
 {
+  if (argc < 3)
+    return -1;
+
   using std::string;
   using std::vector;
   string s(argv[1]);
@@ -22,10 +36,12 @@ int main(int argc, char *argv[])
     printf("'%s'", it->c_str());
   }
   printf("]\n");
-  string cmd("python -c'print \"");
+  string cmd("python3 -c'print(\"");
   cmd += s;
   cmd += "\".split(\"";
   cmd += sep;
-  cmd += "\")'";
+  cmd += "\"))'";
   system(cmd.c_str());
+
+  return 0;
 }
