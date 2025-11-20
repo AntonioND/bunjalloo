f8c403f2e2bca53ea66ef952e98f6c167f3a2c80
Antonio Niño Díaz <antonio_nd@outlook.com>
2025-11-20 02:35:24 +0000

    tests: ISO: Fix and improve ISO 8859-1 decoding
    Commit a231f3b5dcf9 ("ISO: Fix decoding of ISO 8859-1 text") actually
    broke it.
    
    This patch fixes it, documents it, and fixes the "parser" tests.

--- bunjalloo/bwt/source/ISO_8859_1.cpp
+++ bunjalloo/bwt/source/ISO_8859_1.cpp
@@ -1,55 +1,69 @@
 #include <stdio.h>
 #include "ISO_8859_1.h"
+
+// Use the unicode replacement character for unused encodings
+#define UNUSED 0xFFFD
+
 const unsigned short s_1252[] = {
-    127, //127
- 0x20AC, //128
-    129, //129
- 0x201A, //130
- 0x0192, //131
- 0x201E, //132
- 0x2026, //133
- 0x2020, //134
- 0x2021, //135
- 0x02C6, //136
- 0x2030, //137
- 0x0160, //138
- 0x2039, //139
- 0x0152, //140
-    141, //141
- 0x017D, //142
-    143, //143
-    144, //144
- 0x2018, //145
- 0x2019, //146
- 0x201C, //147
- 0x201D, //148
- 0x2022, //149
- 0x2013, //150
- 0x2014, //151
- 0x02DC, //152
- 0x2122, //153
- 0x0161, //154
- 0x203A, //155
- 0x0153, //156
-    157, //157
- 0x017E, //158
- 0x0178, //159
+ UNUSED, // 127
+ 0x20AC, // 128 - €
+ UNUSED, // 129
+ 0x201A, // 130 - ‚
+ 0x0192, // 131 - ƒ
+ 0x201E, // 132 - „
+ 0x2026, // 133 - …
+ 0x2020, // 134 - †
+ 0x2021, // 135 - ‡
+ 0x02C6, // 136 - ˆ
+ 0x2030, // 137 - ‰
+ 0x0160, // 138 - Š
+ 0x2039, // 139 - ‹
+ 0x0152, // 140 - Œ
+ UNUSED, // 141
+ 0x017D, // 142 - Ž
+ UNUSED, // 143
+ UNUSED, // 144
+ 0x2018, // 145 - ‘
+ 0x2019, // 146 - ’
+ 0x201C, // 147 - “
+ 0x201D, // 148 - ”
+ 0x2022, // 149 - •
+ 0x2013, // 150 - –
+ 0x2014, // 151 - —
+ 0x02DC, // 152 - ˜
+ 0x2122, // 153 - ™
+ 0x0161, // 154 - š
+ 0x203A, // 155 - ›
+ 0x0153, // 156 - œ
+ UNUSED, // 157
+ 0x017E, // 158 - ž
+ 0x0178, // 159 - Ÿ
 };
 
 unsigned short ISO_8859_1::decode(unsigned short value)
 {
-  if ( (value > 0x00 and value < 0x7F))
+  if (value > 0x00 and value < 0x7F)
   {
+    // Standard ASCII values are converted directly to Unicode code points
     return value;
   }
-  else if (value >= 0x7F  and value <= 0x9F)
+  else if (value >= 0x7F and value <= 0x9F)
   {
-    // windows 1252 code pages
-    return s_1252[value-0x7f];
+    // The values in this range aren't part of ISO 8859-1, so we use the
+    // values of the Windows 1252 code page.
+    return s_1252[value - 0x7f];
+  }
+  else if (value >= 0xA0 and value <= 0xFF)
+  {
+    // The code points of the characters used in Latin-1 have the same values
+    // as their Unicode encodings:
+    // https://en.wikipedia.org/wiki/Latin-1_Supplement
+    return value;
   }
   else
   {
-    return 0xFFFD;
+    // This shouldn't happen, the values are always 8-bit wide
+    return UNUSED;
   }
 }
 
