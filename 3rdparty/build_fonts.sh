63c9a403eadf72fa98c28e9cb9be726762ae6fc8
Antonio Niño Díaz <antonio_nd@outlook.com>
2025-11-21 00:45:07 +0000

    libfont: Update binary data with new font

--- 3rdparty/build_fonts.sh
+++ 3rdparty/build_fonts.sh
@@ -6,11 +6,15 @@
 FONTS=dejavu-fonts-ttf-2.37/ttf/
 OUT=../nitrofs/fonts/
 
+LIBFONT=../libfont/fonts/
+
 ../tools/convertftf/convertftf \
     --map=$OUT/sans.map --set=$OUT/sans.set \
     --size=11 \
     --offset_ascender=3 \
     --offset_descender=-1 \
     $FONTS/DejaVuSans.ttf
+
+cp $OUT/sans.map $OUT/sans.set $LIBFONT
 
 exit 0
