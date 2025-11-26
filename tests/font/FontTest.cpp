dd50e6beb71b4cf43459db8d311844c2ecc96ba3
Antonio Niño Díaz <antonio_nd@outlook.com>
2025-11-26 01:07:26 +0000

    tests: Update values for font test
    After migrating from Bitstream Vera to DejaVu the tests broke. They have
    been updated to match the values of the new font and detect unexpected
    changes in the future.

--- tests/font/FontTest.cpp
+++ tests/font/FontTest.cpp
@@ -29,28 +29,28 @@ class FontTest: public testing::Test
 TEST_F(FontTest, check_height_is_overall_max)
 {
   int result = m_font->height();
-  EXPECT_EQ(12, result);
+  EXPECT_EQ(14, result);
 }
 
 TEST_F(FontTest, findEnd_no_limits)
 {
   /*
-    For 11 point serif, these are the sizes of abcd:
-    a 1679
-    b 1803
-    c 1577
-    d 1803
-    total = 6862
+    For the current font, these are the sizes of abcd:
+    a 1726
+    b 1787
+    c 1548
+    d 1787
+    total = 6848
    */
   unsigned int result;
   EXPECT_TRUE(m_font->findEnd("abcd", 0, 0, &result, 0));
-  EXPECT_EQ(6862U, result);
+  EXPECT_EQ(6848U, result);
 }
 
 TEST_F(FontTest, findEnd_char_limit)
 {
   /*
-    For 11 point serif, these are the sizes of that string:
+    For the current font, these are the sizes of that string:
     (C) = 2816
     ' ' = 895
     1 = 1792
@@ -69,76 +69,51 @@ TEST_F(FontTest, findEnd_char_limit)
 TEST_F(FontTest, findEnd_size_limit)
 {
   const char test[] = "ÙÙØØ<=>nopq<=>ØØÙÙ";
-  /*
-    For 11 point serif, these are the sizes of that string:
-      < 2360
-      = 2360
-      > 2360
-      n 1814
-      o 1695
-      p 1803
-      q 1803
-      Ø 2309
-      Ù 2373
-
-      2373*2 + 2309*2 + 2360*2 + 1814 + 1695 + 1803 + 1803 + 2360*3 + 2309*2 + 2373*2
-      = 39997 (= 156 pixels, more or less)
-   */
+  // For the current font, this is the size of the string.
   unsigned int result;
   EXPECT_TRUE(m_font->findEnd(test, 0, 0, &result, 0));
-  EXPECT_EQ(40003, (int)result);
+  EXPECT_EQ(38344, (int)result);
 
-  // check that 90 pixels in it stops
-  // 90 << 8 == 23040
-  // 2373*2 + 2309*2 + 2360*3 + 1814 + 1695 + 1803 = 21756, add the q and it goes over
+  // Check that 90 pixels in it stops (90 << 8 == 23040)
+  // The current string doesn't fit in that space
   unsigned int lastChar;
   EXPECT_FALSE(m_font->findEnd(test, 90, 0, &result, &lastChar));
-  EXPECT_EQ(21756, (int)result);
-  EXPECT_EQ(10U, lastChar);
+  EXPECT_EQ(22713, (int)result);
+  EXPECT_EQ(11U, lastChar);
 }
 
 TEST_F(FontTest, findEnd_both_limit)
 {
   const char test[] = "abcd abcd abcd";
-  /*
-    For 11 point serif, these are the sizes of that string:
-    ' ' = 895
-    a 1679
-    b 1803
-    c 1577
-    d 1803
-
-    total = 6862 * 3 + 895 * 2
-    = 22376 = 87
-   */
+  // For the current font, these are the sizes of that string:
   unsigned int result;
   EXPECT_TRUE(m_font->findEnd(test, 0, 0, &result, 0));
-  EXPECT_EQ(6862 * 3 + 895 * 2, (int)result);
+  EXPECT_EQ(22334, (int)result);
 
   unsigned int lastChar;
   // abcd a
   // 6862 + 895 + 1679 = 9436
   EXPECT_FALSE(m_font->findEnd(test, 37, 9, &result, &lastChar));
-  EXPECT_EQ(9436, (int)result);
+  EXPECT_EQ(9469, (int)result);
   EXPECT_EQ(6U, lastChar);
 }
 
 TEST_F(FontTest, test_valueToIndex) {
   TestCodePointToIndex(3, ' ');
   TestCodePointToIndex(36, 'A');
-  TestCodePointToIndex(245, 190);
-  TestCodePointToIndex(186, 255);
-  TestCodePointToIndex(226, 322);
-  TestCodePointToIndex(155, 960);
-  TestCodePointToIndex(178, 8211);
-  TestCodePointToIndex(156, 8747);
-  TestCodePointToIndex(185, 9674);
-  TestCodePointToIndex(192, 64257);
-  TestCodePointToIndex(193, 64258);
+  TestCodePointToIndex(128, 190);
+  TestCodePointToIndex(193, 255);
+  TestCodePointToIndex(260, 322);
+  TestCodePointToIndex(853, 960);
+  TestCodePointToIndex(2806, 8211);
+  TestCodePointToIndex(3254, 8747);
+  TestCodePointToIndex(3746, 9674);
+  TestCodePointToIndex(5042, 64257);
+  TestCodePointToIndex(5043, 64258);
   TestCodePointToIndex(-1, 1);
-  TestCodePointToIndex(-1, 256);
-  TestCodePointToIndex(-1, 264);
-  TestCodePointToIndex(-1, 64259);
+  TestCodePointToIndex(194, 256);
+  TestCodePointToIndex(202, 264);
+  TestCodePointToIndex(5044, 64259);
 }
 
 TEST_F(FontTest, nonvalid) {
